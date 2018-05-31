fcd3ce02c06ef74dc803841bec2efa19cbfeb527
Tim Bergsma <tim.bergsma@certara.com>
2018-05-31 09:40:19 -0400

    updated description

--- R/multiplot.R
+++ R/multiplot.R
@@ -1,6 +1,6 @@
-#' Arrange Multiple Trellis Plots in a Grid
+#' Arrange Multiple Trellis or GG Plots in a Grid
 #'
-#' Arranges mutiple trellis plots or ggpots (all of the same type) in a grid, automatically choosing number of rows and columns.  By default, number of rows is one less than or equal to the number of columns.
+#' Arranges mutiple trellis plots or ggplots in a grid, automatically choosing number of rows and columns.  By default, number of rows is one less than or equal to the number of columns.
 #'
 #' @export
 #' @param ... trellis objects
@@ -31,8 +31,8 @@ multiplot <- function(..., nrow = NULL, ncol = NULL){
   len <- length(x)
   nms <- seq_along(x)
   class <- unique(sapply(x,class))
-  if(!is.null(class))stopifnot(all(sapply(x,inherits,'trellis')) | all(sapply(x,inherits,'ggplot')))
-  gg <- inherits(x[[1]],'ggplot')
+  if(!is.null(class))stopifnot(sapply(x,inherits,'trellis') | sapply(x,inherits,'ggplot'))
+  #gg <- inherits(x[[1]],'ggplot')
   root <- sqrt(len)
   if(!is.null(nrow)){
     if(is.null(ncol)) {
@@ -47,21 +47,22 @@ multiplot <- function(..., nrow = NULL, ncol = NULL){
   if(is.null(nrow)) nrow <- ceiling(len/ncol)
   # now nrow is defined for sure
 
-  if(gg)return(do.call(grid.arrange, c(x,list(ncol = ncol))))
-
-  y <- expand.grid(
-    run = seq_len(ncol),
-    rise = seq_len(nrow)
-  )
-  stopifnot(nrow(y) >= len)
-  y <- y[nms,]
-  printOne <- function(z,x,y,nx,ny,more)print(z,split=c(x,y,nx,ny),more = more)
-  for(i in nms)printOne(
-    z = x[[i]],
-    x = y[i,'run'],
-    y = y[i,'rise'],
-    nx = ncol,
-    ny = nrow,
-    more = !(i == len)
-  )
+  #if(gg)return(do.call(grid.arrange, c(x,list(ncol = ncol))))
+  return(do.call(grid.arrange, c(x,list(ncol = ncol))))
+#
+#   y <- expand.grid(
+#     run = seq_len(ncol),
+#     rise = seq_len(nrow)
+#   )
+#   stopifnot(nrow(y) >= len)
+#   y <- y[nms,]
+#   printOne <- function(z,x,y,nx,ny,more)print(z,split=c(x,y,nx,ny),more = more)
+#   for(i in nms)printOne(
+#     z = x[[i]],
+#     x = y[i,'run'],
+#     y = y[i,'rise'],
+#     nx = ncol,
+#     ny = nrow,
+#     more = !(i == len)
+#   )
 }
